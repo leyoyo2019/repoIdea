87ffb187e6d5636f0fd54ab3ee45e2e892b3195e
jianghao <jianghao@itcast.cn>
2019-06-07 23:43:41 +0800

    sdfghj2222222222a

---
+++ gitprj/src/main/java/com/itheima/git/A.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+/**
+ * Created by lenovo on 2019/6/7.
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("dev........");
+    }
+}
