81cfbdf527a78d69b82168575da976b77bd816e5
jianghao <jianghao@itcast.cn>
2019-06-07 22:48:35 +0800

    commit gitprj to local repository

---
+++ gitprj/src/main/java/com/itheima/git/GitClass.java
@@ -0,0 +1,19 @@
+package com.itheima.git;
+
+/**
+ * Created by lenovo on 2019/6/7.
+ */
+public class GitClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+    }
+}
